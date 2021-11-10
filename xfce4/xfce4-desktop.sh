77b9415469600678fefbf6976e376476757236b3
Eroldin <54679426+Eroldin@users.noreply.github.com>
2021-11-10 23:59:02 +0100

    Update xfce4-desktop.sh

--- xfce4/xfce4-desktop.sh
+++ xfce4/xfce4-desktop.sh
@@ -2,26 +2,27 @@
 
 scriptuser="$USER"
 echo "permit persist :wheel" | sudo tee /etc/doas.conf >/dev/null
-trizen -S opendoas opendoas-sudo autofs --noedit --skipinteg
-doas rm -rf /etc/sudoers.*
+trizen -S opendoas opendoas-sudo --noedit
+trizen -S autofs --noedit --skipinteg
+doas rm -rf /etc/sudoers.* -v
 trizen -Rns xarchiver mupdf mousepad xfce4-taskmanager
-# gpg --import /Basic_Arch/spotify.gpg/pubkey_0D811D58.gpg
-trizen -S engrampa atril gnome-system-monitor xed onlyoffice-bin masterpdfeditor-free plank vimix-icon-theme dracula-gtk-theme whatsapp-for-linux spotify hunspell-nl aspell-en aspell-nl lightdm-gtk-greeter-settings pipewire pipewire-pulse gst-plugin-pipewire blueberry ttf-roboto popsicle-bin --noedit
+trizen -S engrampa atril gnome-system-monitor xed onlyoffice-bin masterpdfeditor-free plank vimix-icon-theme dracula-gtk-theme whatsapp-for-linux hunspell-nl aspell-en aspell-nl lightdm-gtk-greeter-settings pipewire pipewire-pulse gst-plugin-pipewire blueberry ttf-roboto popsicle-bin --noedit
 doas pacman -Rns $(pacman -Qdqt)
 doas pacman -Syu
 trizen -Sc
 doas usermod "$scriptuser" -aG rfkill
 doas localectl set-x11-keymap us pc105 intl
-git clone https://github.com/dracula/plank /tmp/plank >/dev/null 2>&1
-doas mv /tmp/plank/Dracula /usr/share/plank/themes/
+git clone https://github.com/dracula/plank /tmp/plank
+doas mv /tmp/plank/Dracula /usr/share/plank/themes/ -v
 doas chown root:root /usr/share/plank/themes/Dracula
-doas mv /Basic_Arch/*.jpg /usr/share/backgrounds
-doas chmod 644 /usr/share/backgrounds/*.jpg
-doas chmod 755 /usr/share/plank/themes/Dracula
-doas chmod 644 /usr/share/plank/themes/Dracula/*
-mkdir -p "$HOME/.config/Thunar"
-mkdir -p "$HOME/.config/gtk-3.0"
+doas mv /Basic_Arch/*.jpg /usr/share/backgrounds -v
+doas chmod 644 /usr/share/backgrounds/*.jpg -v
+doas chmod 755 /usr/share/plank/themes/Dracula -v
+doas chmod 644 /usr/share/plank/themes/Dracula/* -v
+mkdir -vp "$HOME/.config/Thunar"
+mkdir -v "$HOME/.config/gtk-3.0"
 
+echo "Creating usefull aliases for zsh"
 cat <<-\EOF >> "$HOME/.zshrc"
 
 	alias trizen="trizen --noedit"
@@ -30,11 +31,7 @@ cat <<-\EOF >> "$HOME/.zshrc"
 	}
 EOF
 
-cat <<-\EOF >> "$HOME/.dmrc"
-	[Desktop]
-	Session=xfce
-EOF
-
+echo "Creating Thunar actions..."
 cat <<-\EOF > "$HOME/.config/Thunar/uca.xml"
 	<?xml version="1.0" encoding="UTF-8"?>
 	<actions>
@@ -63,10 +60,11 @@ cat <<-\EOF > "$HOME/.config/Thunar/uca.xml"
 	</actions>
 EOF
 
+"Crating basic bookmarks for thunar"
 cat <<-EOF >> "$HOME/.config/gtk-3.0/bookmarks"
-	file:///home/$scriptuser/Documents
-	file:///home/$scriptuser/Pictures
-	file:///home/$scriptuser/Music
-	file:///home/$scriptuser/Videos
-	file:///home/$scriptuser/Downloads
+	"file:///home/$scriptuser/Documents"
+	"file:///home/$scriptuser/Pictures"
+	"file:///home/$scriptuser/Music"
+	"file:///home/$scriptuser/Videos"
+	"file:///home/$scriptuser/Downloads"
 EOF
